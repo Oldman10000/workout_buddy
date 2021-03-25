f79f9091df115344090c33615dace0f4752358ea
oldman10000 <patrickoldman1993@gmail.com>
2021-03-25 15:25:20 +0000

    Added nav menu with styling and dropdown functionality

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,14 @@
 $(".toggle-trigger").click(function () {
   $(this).toggleClass('active').next().slideToggle('slow');
 });
+
+// navbar toggle
+
+$("#hamburger").click(function () {
+  let x = document.querySelector('#myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+});
