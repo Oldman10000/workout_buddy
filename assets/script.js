6e69eb53cb102b6f8361df97bba24764aa8762f1
oldman10000 <patrickoldman1993@gmail.com>
2021-03-25 16:05:50 +0000

    Added scrollTop button to return user to top

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,23 @@ $("#hamburger").click(function () {
     x.style.display = 'block';
   }
 });
+
+// Return to top button script copied and adapted from W3 schools tutorial
+// https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+mybutton = document.querySelector('.circle');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+$("#toTop").click(function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+})
