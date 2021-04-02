cb741f12053bf59bd7833b88ae83fb921b1876c9
oldman10000 <patrickoldman1993@gmail.com>
2021-04-02 15:50:56 +0100

    Added side navbar element

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,8 @@
-// navbar toggle - code copied and adapted from W3 schools tutorial
-// https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
-
-function toggleNav() {
-  let x = document.querySelector('#myLinks');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
-  }
-}
-
-$("#hamburger, .section-link").click(function () {
-  toggleNav();
+// navbar toggle
+$("#hamburger").click(function () {
+  $(this).toggleClass('flip');
+  $(".nav").toggleClass('visible');
+  $("main").toggleClass('shrink');
 });
 
 // toggles display of workout instructions
@@ -19,20 +10,6 @@ $(".toggle-trigger").click(function () {
   $(this).toggleClass('active').next().slideToggle('slow');
   $(this).parent().toggleClass('green');
 });
-
-// Return to top button script copied and adapted from W3 schools tutorial
-// https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-
-mybutton = document.querySelector('.circle');
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = () => {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 // empty array of exercises on workout list
 let exercises = [];
@@ -529,6 +506,20 @@ $("a").click(function (e) {
     });
   } // End if
 });
+
+// Return to top button script copied and adapted from W3 schools tutorial
+// https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+mybutton = document.querySelector('.circle');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
 // smooth function copied and adapted from tutorialdeep.com
 // https://tutorialdeep.com/knowhow/smooth-scroll-to-top-jquery/
